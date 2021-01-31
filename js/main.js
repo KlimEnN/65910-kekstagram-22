d1b2d37ddd0fafb1e4b26b0b5af663d147fe67be
KlimEnN <aiklimenk@gmail.com>
2021-01-31 18:28:05 +0200

    Добали функцию, которая проверят количество знаков в строке

--- js/main.js
+++ js/main.js
@@ -6,8 +6,24 @@ function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   } else {
     // eslint-disable-next-line no-console
-    console.log('error')
+    console.log('error');
   }
 }
 // eslint-disable-next-line no-console
 console.log(getRandomInRange(10, 100));
+
+//Функция, которая будет считать количество символов в поле комментариев
+//Переменная, которая задает допустимое количество символов в коммментрии. Логично использовать константу
+const MAX_ALLOWED_COMMENT_LENGTH = 140;
+//Переменная, которя используется для тестирования функции. Далее мы будем получать строку от пользователя
+const COMMENT_STRING = 'Комментарий';
+
+// Функция, которая проверяет длинну комментария
+const isAllowedString = function(COMMENT_STRING, MAX_ALLOWED_COMMENT_LENGT) {
+  if (COMMENT_STRING.length <= MAX_ALLOWED_COMMENT_LENGTH) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
