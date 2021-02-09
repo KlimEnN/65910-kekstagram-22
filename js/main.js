844a10ad07333a589226421b1012849ac849bdfd
Andriy <aiklimenk@gmail.com>
2021-02-09 22:55:32 +0200

    Заготовка для ДЗ

--- js/main.js
+++ js/main.js
@@ -9,22 +9,76 @@ function getRandomInRange(min, max) {
     console.log('error');
   }
 }
+
+
 // eslint-disable-next-line no-console
-console.log(getRandomInRange(10, 100));
+// console.log(getRandomInRange(10, 100));
+
+
 
 //Функция, которая будет считать количество символов в поле комментариев
 //Переменная, которая задает допустимое количество символов в коммментрии. Логично использовать константу
 const MAX_ALLOWED_COMMENT_LENGTH = 140;
 //Переменная, которя используется для тестирования функции. Далее мы будем получать строку от пользователя
+// eslint-disable-next-line no-unused-vars
 const COMMENT_STRING = 'Комментарий';
 
 // Функция, которая проверяет длинну комментария
+// eslint-disable-next-line no-unused-vars
 const isAllowedString = function(COMMENT_STRING, MAX_ALLOWED_COMMENT_LENGT) {
   if (COMMENT_STRING.length <= MAX_ALLOWED_COMMENT_LENGTH) {
     return true;
   } else {
     return false;
   }
+};
+
+const ID = [1, 2]
+
+const DESCRIPTIONS = [
+  'описание фото #1',
+  'описание фото #2',
+  'описание фото #3',
+  'описание фото #4',
+  'описание фото #5'
+  ];
+
+const NAMES = [
+  'Андрей',
+  'Маша',
+  'Максим',
+  'Олег',
+  'Оля'
+]
+
+const COMMENTS = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+]
+
+const createArr = () => {
+  let randomIndex = getRandomInRange(0, ID.length - 1);
+
+  console.log(randomIndex)
+
+
+  return  {
+    id:ID[getRandomInRange],
+    url: '',
+    description: DESCRIPTIONS[getRandomInRange],
+    likes: ''
+  }
+
 }
 
-test()
+
+
+
+
+// const createArr2 = () => {
+//
+// }
